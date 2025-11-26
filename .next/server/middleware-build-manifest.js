f6be2a12f20685707a55824a08cdb747b30064c0
phil842 <philip.kasselman@glasshouse.qld.edu.au>
2025-11-26 04:16:49 +0000

    chore(images): remove temporary artifact public/images/ergfds.png

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,28 @@
-self.__BUILD_MANIFEST={"polyfillFiles":["static/chunks/polyfills.js"],"devFiles":["static/chunks/react-refresh.js"],"ampDevFiles":[],"lowPriorityFiles":["static/development/_buildManifest.js","static/development/_ssgManifest.js"],"rootMainFiles":[],"pages":{"/":["static/chunks/webpack.js","static/chunks/main.js","static/chunks/pages/index.js"],"/_app":["static/chunks/webpack.js","static/chunks/main.js","static/chunks/pages/_app.js"],"/_error":["static/chunks/webpack.js","static/chunks/main.js","static/chunks/pages/_error.js"]},"ampFirstPages":[]}
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/": [
+      "static/chunks/cbc479571bcdd928.js",
+      "static/chunks/f074cdbbcec5ccc4.js",
+      "static/chunks/turbopack-dca6de642a72c80a.js"
+    ],
+    "/_app": [
+      "static/chunks/f947cd8da1440284.js",
+      "static/chunks/f074cdbbcec5ccc4.js",
+      "static/chunks/a383dbfa24ccf2e1.css",
+      "static/chunks/turbopack-2db82b62b6d7ff45.js"
+    ],
+    "/_error": [
+      "static/chunks/5c948ca1671dc2fc.js",
+      "static/chunks/f074cdbbcec5ccc4.js",
+      "static/chunks/turbopack-58ab578cd7f094fe.js"
+    ]
+  },
+  "devFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
