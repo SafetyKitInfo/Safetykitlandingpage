383d93861adb240fb46791134b58499439797f83
Safety Kit <info.safetykit@gmail.com>
2025-12-15 01:16:46 +0000

    Remove bottom padding from scroll animation to eliminate blank space

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -4,16 +4,16 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/01a684b2cec8fccf.js",
       "static/chunks/3cf4fc7bcf7b8fa2.js",
       "static/chunks/3934f1032d10c738.js",
-      "static/chunks/0c51b866c4b0dafe.js",
+      "static/chunks/e717725a027942c8.js",
       "static/chunks/a407c92e68a96beb.js",
-      "static/chunks/turbopack-afa2d91a6ce6100c.js"
+      "static/chunks/turbopack-25d713109c540855.js"
     ],
     "/_app": [
       "static/chunks/0d4c3428c53b34e8.js",
       "static/chunks/3934f1032d10c738.js",
       "static/chunks/a407c92e68a96beb.js",
-      "static/chunks/c4ce6c54b8ffe535.css",
-      "static/chunks/turbopack-0f45d08a2bfca247.js"
+      "static/chunks/2efb4d4600a448b7.css",
+      "static/chunks/turbopack-a36da075e12370de.js"
     ],
     "/_error": [
       "static/chunks/a2a580d43b0f834e.js",
@@ -24,9 +24,9 @@ globalThis.__BUILD_MANIFEST = {
     "/aurora-demo": [
       "static/chunks/20f5dd888b4f5543.js",
       "static/chunks/a407c92e68a96beb.js",
-      "static/chunks/3c9180b54b03fedb.js",
+      "static/chunks/e581e1da9c6af70b.js",
       "static/chunks/3934f1032d10c738.js",
-      "static/chunks/turbopack-9b04e28c56722d01.js"
+      "static/chunks/turbopack-04d4ff15ad93e965.js"
     ]
   },
   "devFiles": [],
