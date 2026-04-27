a245c30e7a25185e0a329c605252d7035ea35626
Safety Kit <info.safetykit@gmail.com>
2026-04-27 07:09:43 +0000

    Remove generated .next build artifacts from repo

--- .next/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e81a3f3c._.js")
-R.c("server/chunks/ssr/node_modules_next_3be9481d._.js")
-R.m(39141)
-module.exports=R.m(39141).exports
--- .next/server/pages/_error.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
-R.c("server/chunks/ssr/node_modules_next_55786baf._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__84687bd7._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e81a3f3c._.js")
-R.c("server/chunks/ssr/node_modules_next_3be9481d._.js")
-R.c("server/chunks/ssr/_79ab815e._.js")
-R.m(10910)
-module.exports=R.m(10910).exports
